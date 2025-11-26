af86ea0b6a08b9d84f2ef4c655a50cdc99cd4101
Mike Jensen <jensen.mike57@gmail.com>
2025-11-26 19:32:52 +0100

    wait for network to be ready before starting

--- components/sensor_tommy/sensor_tommy.cpp
+++ components/sensor_tommy/sensor_tommy.cpp
@@ -1,38 +1,45 @@
-#include "esphome/core/log.h"
 #include "sensor_tommy.h"
-#include "lib.h"
+
 #include <cstring>
+
+#include "esphome/components/network/util.h"
+#include "esphome/core/log.h"
+#include "lib.h"
 
 namespace esphome {
 namespace sensor_tommy {
 
 static const char *TAG = "sensor_tommy.component";
 
-float SensorTommy::get_setup_priority() const {
-    return setup_priority::LATE;
-}
+float SensorTommy::get_setup_priority() const { return setup_priority::LATE; }
 
-void SensorTommy::setup(){
-    if (!instance_ip_.empty() && this->file_server_http_port_ != 0 && this->file_server_https_port_ != 0) {
-        tommy_start_with_instance_details(this->instance_ip_.c_str(), this->file_server_http_port_, this->file_server_https_port_);
-    } else {
-        tommy_start();
-    }
-}
+void SensorTommy::setup() {}
 
 void SensorTommy::loop() {
+  if (network::is_connected()) {
+    if (!instance_ip_.empty() && this->file_server_http_port_ != 0 && this->file_server_https_port_ != 0) {
+      tommy_start_with_instance_details(this->instance_ip_.c_str(), this->file_server_http_port_,
+                                        this->file_server_https_port_);
+    } else {
+      tommy_start();
+    }
+
+    this->disable_loop();
+
+    ESP_LOGI(TAG, "TOMMY Sensor started");
+  }
 }
 
 void SensorTommy::dump_config() {
-    ESP_LOGCONFIG(TAG, "TOMMY Sensor:");
-    if (!instance_ip_.empty()) {
-        ESP_LOGCONFIG(TAG, "  Instance IP: %s", this->instance_ip_.c_str());
-        ESP_LOGCONFIG(TAG, "  HTTP Port: %d", this->file_server_http_port_);
-        ESP_LOGCONFIG(TAG, "  HTTPS Port: %d", this->file_server_https_port_);
-    } else {
-        ESP_LOGCONFIG(TAG, "  Using auto-discovery via mDNS");
-    }
+  ESP_LOGCONFIG(TAG, "TOMMY Sensor:");
+  if (!instance_ip_.empty()) {
+    ESP_LOGCONFIG(TAG, "  Instance IP: %s", this->instance_ip_.c_str());
+    ESP_LOGCONFIG(TAG, "  HTTP Port: %d", this->file_server_http_port_);
+    ESP_LOGCONFIG(TAG, "  HTTPS Port: %d", this->file_server_https_port_);
+  } else {
+    ESP_LOGCONFIG(TAG, "  Using auto-discovery via mDNS");
+  }
 }
 
-} 
-}
+}  // namespace sensor_tommy
+}  // namespace esphome
--- components/sensor_tommy/sensor_tommy.h
+++ components/sensor_tommy/sensor_tommy.h
@@ -1,18 +1,19 @@
 #pragma once
 
-#include "esphome/core/component.h"
 #include <string>
+
+#include "esphome/core/component.h"
 
 namespace esphome {
 namespace sensor_tommy {
 
 class SensorTommy : public Component {
  public:
-  float get_setup_priority() const override; 
+  float get_setup_priority() const override;
   void setup() override;
   void loop() override;
   void dump_config() override;
-  
+
   void set_instance_ip(const std::string &ip) { this->instance_ip_ = ip; }
   void set_file_server_http_port(int port) { this->file_server_http_port_ = port; }
   void set_file_server_https_port(int port) { this->file_server_https_port_ = port; }
@@ -23,5 +24,5 @@ class SensorTommy : public Component {
   int file_server_https_port_ = 0;
 };
 
-} 
-}
+}  // namespace sensor_tommy
+}  // namespace esphome
